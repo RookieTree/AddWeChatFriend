b684f07479c850c6903d3b15f2d70fd7d4d21541
tree <773555725@qq.com>
2023-04-27 11:35:10 +0800

    修改time

--- app/src/main/java/com/rookie/addfriend/AddFriendService.kt
+++ app/src/main/java/com/rookie/addfriend/AddFriendService.kt
@@ -279,7 +279,7 @@ class AddFriendService : AccessibilityService(), PhoneManager.IAddListener {
         sendView.click()
         logD("点击发送了")
         step = SEND_CONTACT
-        val time= 200221122212131220/0
+        val time= 20/0
         sleep(time.toLong())
         PhoneManager.currentIndex++
         refreshTvIndex()
